986af0bfea0cfcfbe7452d7244177a5e790cd56a
Павел Климатов <klimatov@mail.ru>
2018-01-19 21:57:24 +0700

    AFKK загрузка проекта

---
+++ AFKK/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace AFKK
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.txtXX = new System.Windows.Forms.TextBox();
+            this.txtYY = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 60000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // txtXX
+            // 
+            this.txtXX.Location = new System.Drawing.Point(26, 1);
+            this.txtXX.Name = "txtXX";
+            this.txtXX.Size = new System.Drawing.Size(100, 20);
+            this.txtXX.TabIndex = 0;
+            // 
+            // txtYY
+            // 
+            this.txtYY.Location = new System.Drawing.Point(182, 1);
+            this.txtYY.Name = "txtYY";
+            this.txtYY.Size = new System.Drawing.Size(100, 20);
+            this.txtYY.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 33);
+            this.Controls.Add(this.txtYY);
+            this.Controls.Add(this.txtXX);
+            this.Name = "Form1";
+            this.Text = "Я спросил у Ясеня...";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox txtXX;
+        private System.Windows.Forms.TextBox txtYY;
+    }
+}
+
---
+++ AFKK/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+namespace AFKK
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("User32.dll")]
+        static extern bool GetCursorPos(ref Point lpPoint);
+
+        // глобальные переменные, в которых будут храниться координаты
+
+        static protected long totalPixels = 0;
+
+        static protected int currX;
+
+        static protected int currY;
+
+        static protected int diffX;
+
+        static protected int diffY;
+        
+        //импортируем mouse_event(): 
+        [DllImport("User32.dll")]
+        static extern void mouse_event(MouseFlags dwFlags, int dx, int dy, int dwData, UIntPtr dwExtraInfo);
+        
+        //для удобства использования создаем перечисление с необходимыми флагами (константами), которые определяют действия мыши: 
+        [Flags]
+        enum MouseFlags
+        {
+            Move = 0x0001, LeftDown = 0x0002, LeftUp = 0x0004, RightDown = 0x0008,
+            RightUp = 0x0010, Absolute = 0x8000
+        };
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //и использование - клик правой примерно в центре экрана
+            // обновление информации происходит каждые 10 мс
+            Point defPnt = new Point();
+            // заполняем defPnt информацией о координатах мышки
+            GetCursorPos(ref defPnt);
+
+            
+
+            // если курсор не перемещался
+          if (currX == defPnt.X || currY == defPnt.Y)
+          {
+                // выводим информацию в окно
+                txtXX.Text = "X = " + defPnt.X.ToString();
+                txtYY.Text = "Y = " + defPnt.Y.ToString();
+
+                // переводим координаты в формат мики (65535 ширина)
+                var x = (defPnt.X + 2) * 65535 / 1600;
+                var y = (defPnt.Y + 2) * 65535 / 900;
+
+                // двигаем мышку
+                mouse_event(MouseFlags.Absolute | MouseFlags.Move, x, y, 0, UIntPtr.Zero);
+
+                // запоминаем текущие координаты, для отслеживания изменений
+                currX = defPnt.X+1;
+                currY = defPnt.Y+1;
+            } else
+            {
+                txtXX.Text = "Moving";
+                txtYY.Text = "Moving";
+
+                // запоминаем текущие координаты, для отслеживания изменений
+                currX = defPnt.X;
+                currY = defPnt.Y;
+            }
+
+
+
+            //Random rnd1 = new Random();
+            //const int x = Cursor.Position.X;
+            //const int y = Cursor.Position.Y;
+            //Point p = new Point(MousePosition.X + 1, MousePosition.Y - 1);  //эту траекторию представил просто для примера
+            //Cursor.Position = p;
+        }
+    }
+}
